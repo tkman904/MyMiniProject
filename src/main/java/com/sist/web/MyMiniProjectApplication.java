3ba4e802503e3cfcec9bd20ad06b7f04b4101b0b
tkman904 <cjfdn0904@gmail.com>
2025-12-16 16:09:57 +0900

    부트_8일차 (98일차)

---
+++ src/main/java/com/sist/web/MyMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMiniProjectApplication.class, args);
+	}
+
+}
